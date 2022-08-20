9e2dac4ec1dc2a3752909be025646e7768972e7a
Jonatas Nascimento <jonatas.nascimento@elisbrasil.onmicrosoft.com>
2022-08-20 13:38:20 -0300

    dev: adição de valores padrões CNX criação

--- View/FormConfigurarCNX.cs
+++ View/FormConfigurarCNX.cs
@@ -19,6 +19,38 @@ namespace AntennaSetupAPP.View
 
         private void FormConfigurarCNX_Load(object sender, EventArgs e)
         {
+            LoadValuesCNX();
+        }
+
+        private void LoadValuesCNX()
+        {
+            radioButtonTMR.Checked = true;
+            textBoxName.Text = "EDGE50AID";
+            textBoxIPAddress.Text = "";
+
+            comboBoxIPPort.Items.Add("5084");
+            comboBoxIPPort.Items.Add("8081");
+            comboBoxIPPort.Text = comboBoxIPPort.Items[0].ToString();
+
+            comboBoxIPPortA.Items.Add("5085");
+            comboBoxIPPortA.Text = comboBoxIPPortA.Items[0].ToString();
+
+            comboBoxComPort.Items.AddRange(new object[] { "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", });
+            comboBoxComPort.Text = comboBoxComPort.Items[0].ToString();
+
+            comboBoxInitialBaudRate.Items.Add("115200");
+            comboBoxInitialBaudRate.Text = comboBoxInitialBaudRate.Items[0].ToString();
+
+            comboBoxFinalBaudRate.Items.Add("FinalBaudRate");
+            comboBoxFinalBaudRate.Text = comboBoxFinalBaudRate.Items[0].ToString();
+
+            textBoxKanbanAntenaList1.Text = "1";
+            textBoxKanbanAntenaList2.Text = "2";
+            textBoxKanbanAntenaList3.Text = "0";
+            textBoxKanbanAntenaList4.Text = "0";
+
+
+
 
         }
     }
