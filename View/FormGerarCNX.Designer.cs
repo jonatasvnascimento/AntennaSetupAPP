d236b6977c82ea5915a78ff115f7ece372c540f4
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2022-09-08 19:40:11 -0300

    DEV: pegando valores do grid

--- View/FormGerarCNX.Designer.cs
+++ View/FormGerarCNX.Designer.cs
@@ -33,7 +33,6 @@ namespace AntennaSetupAPP.View
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -43,6 +42,8 @@ namespace AntennaSetupAPP.View
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AllowUserToResizeColumns = false;
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
@@ -59,15 +60,15 @@ namespace AntennaSetupAPP.View
             this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
             this.Column2,
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.EnableHeadersVisualStyles = false;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -84,15 +85,9 @@ namespace AntennaSetupAPP.View
             this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.RowTemplate.Height = 25;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(863, 554);
+            this.dataGridView1.Size = new System.Drawing.Size(863, 337);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // Column1
-            // 
-            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column1.FillWeight = 50F;
-            this.Column1.HeaderText = "Select";
-            this.Column1.Name = "Column1";
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Column2
             // 
@@ -100,6 +95,7 @@ namespace AntennaSetupAPP.View
             this.Column2.FillWeight = 70F;
             this.Column2.HeaderText = "System";
             this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
             // 
             // Column3
             // 
@@ -107,6 +103,7 @@ namespace AntennaSetupAPP.View
             this.Column3.FillWeight = 200F;
             this.Column3.HeaderText = "Path";
             this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
             // 
             // Column4
             // 
@@ -114,12 +111,14 @@ namespace AntennaSetupAPP.View
             this.Column4.FillWeight = 50F;
             this.Column4.HeaderText = "File";
             this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
             // 
             // Column5
             // 
             this.Column5.FillWeight = 50F;
             this.Column5.HeaderText = "Testing";
             this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
             // 
             // FormGerarCNX
             // 
@@ -131,6 +130,7 @@ namespace AntennaSetupAPP.View
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormGerarCNX";
             this.Text = "FormGerarCNX";
+            this.Load += new System.EventHandler(this.FormGerarCNX_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -139,7 +139,6 @@ namespace AntennaSetupAPP.View
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
--- View/FormGerarCNX.cs
+++ View/FormGerarCNX.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,67 @@ namespace AntennaSetupAPP.View
         public FormGerarCNX()
         {
             InitializeComponent();
-            dataGridView1.Rows.Add(new object[] { "select True", "ASSOCIAÇÃO", "C/COALARFID",   "Arquivo criado","Teste OK" });
-            dataGridView1.Rows.Add(new object[] { "select True", "MERCADO", "C/COALARFID",      "Arquivo criado","Teste OK" });
-            dataGridView1.Rows.Add(new object[] { "select True", "SCANOUT", "C/COALARFID",      "Arquivo criado","Teste OK" });
-            dataGridView1.Rows.Add(new object[] { "select True", "WRITETAG", "C/COALARFID",     "Arquivo criado","Teste OK" });
         }
 
         private void FormGerarCNX_Load(object sender, EventArgs e)
         {
+            getDirectory();
+        }
+
+        private void getDirectory()
+        {
+            try
+            {
+                List<string> nameApplication = new List<string>();
+                List<string> dirApplaication = new List<string>();
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+                string DirectoryApplication = $@"{directoryInfo.Root}\COALARFID";
+
+                if (!Directory.Exists(DirectoryApplication))
+                {
+                    Directory.CreateDirectory(DirectoryApplication);
+                }
+
+                string[] dirs = Directory.GetDirectories(DirectoryApplication);
+
+                foreach (var dir in dirs)
+                {
+                    var dirNameFolder = dir.Split(@"\");
+                    nameApplication.Add(dirNameFolder[3]);
+                    dirApplaication.Add(dir);
+                }
+
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    dataGridView1.Rows.Add(new object[] { nameApplication[i], dirApplaication[i], "Arquivo criado", "Teste OK" });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+            }
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+
+                    MessageBox.Show($"{row.Cells["System"].Value}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+            }
            
         }
     }
 }
+;
